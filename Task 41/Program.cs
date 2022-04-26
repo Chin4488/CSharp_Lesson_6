91e28d69532bc082878a072c8c41059ab799085c
«Igor <talovez@yandex.ru>
2022-04-26 17:28:00 +0300

    initial

---
+++ Task 41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры или задает 
+// самостоятельно M чисел. Посчитайте, сколько чисел больше 0 
+// ввёл пользователь.
+
+Console.WriteLine("Укажите какое количество чисел будет использоваться?");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+int l=0;
+for (int i=0; i<size; i++)
+{
+    Console.WriteLine($"Введите {i+1} число");
+    array[i]=Convert.ToInt32(Console.ReadLine());
+    if (array[i]>0) l=l+1;
+} 
+Console.WriteLine($"Больше 0 введено {l} чисел");
